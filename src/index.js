ff3b8f04584ccf3ebc79efeb6241661cb2e15487
pwhuan <pwh@0809>
2022-12-05 13:30:51 +0800

    资源管理

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import _ from "lodash";
+import "./style.css";
+import Icon from "./icon.png";
 function component() {
   const element = document.createElement("div");
 
   //   lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
 
   return element;
 }
